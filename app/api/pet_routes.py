bcf228cea714bd4618361625c2bf7039d0c46cce
nikos <nikgiannsm@gmail.com>
2024-11-12 17:22:15 +0200

    Added delete pet endpoint.

--- app/api/pet_routes.py
+++ app/api/pet_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify 
 from app.db import connection
-from app.db.queries import INSERT_NEW_PET, GET_ALL_MY_PETS
+from app.db.queries import INSERT_NEW_PET, GET_ALL_MY_PETS, DELETE_PET
 from app.utils import valid_user
 
 pet_bp = Blueprint('pets' , __name__)
@@ -12,9 +12,9 @@ def insert_pet(id):
 
     with connection:
         with connection.cursor() as cursor:
-            result = valid_user(user_id)
+            is_user_valid = valid_user(user_id)
 
-            if not result:
+            if not is_user_valid:
                 return jsonify({"message": "User does not exist."}), 400  
 
             if not data or not all(key in data for key in("pet_name", "pet_weight", "animal_id")):
@@ -26,17 +26,21 @@ def insert_pet(id):
    
             cursor.execute(INSERT_NEW_PET , (pet_name, pet_weight, user_id, animal_id,))
             pet_id = cursor.fetchone()[0]
+
+            if not pet_id:
+                return jsonify({"message": "Failed to add pet."})
+            
             return jsonify({"id": pet_id, "message": "New pet succesfully added."}), 201
 
-@pet_bp.route('/all_my_pets/<int:id>' , methods=["GET"])
+@pet_bp.route('/all_my_pets/<int:id>' , methods=['GET'])
 def get_all_my_pets(id):
     user_id = id
 
     with connection:
         with connection.cursor() as cursor:
-            result = valid_user(user_id)
+            is_user_valid = valid_user(user_id)
 
-            if not result:
+            if not is_user_valid:
                 return jsonify({"message": "User does not exist."}), 400
             
             cursor.execute(GET_ALL_MY_PETS, (user_id,))
@@ -56,6 +60,31 @@ def get_all_my_pets(id):
             ]
 
             return jsonify({"pets": users_pets}), 200
+    
+@pet_bp.route('/delete_pet/<int:id>', methods=['DELETE'])
+def delete_pet(id):
+    data = request.get_json()
+    user_id = id
+
+    with connection:
+        with connection.cursor() as cursor:
+            is_user_valid = valid_user(user_id)
+
+            if not is_user_valid:
+                return jsonify({"message": "User does not exist."}), 400
+            
+            if not data:
+                return jsonify({"message": "Missing data. Bad request."})
+            
+            pet_id = data["pet_id"]
+
+            cursor.execute(DELETE_PET, (pet_id, user_id,))
+            result = cursor.rowcount
+
+            if not result:
+                return jsonify({"message": f"Failed to delete pet with id:{pet_id}"}), 404
+            
+            return jsonify({"message": "Pet deleted succesfully."}), 201
             
             
 
--- app/db/queries.py
+++ app/db/queries.py
@@ -1,2 +1,3 @@
 INSERT_NEW_PET = 'INSERT INTO pets(name, weight, user_id, animal_id) VALUES(%s, %s, %s, %s) RETURNING id'
 GET_ALL_MY_PETS = 'SELECT * FROM pets WHERE user_id = %s'
+DELETE_PET ="DELETE FROM pets WHERE id = %s AND user_id = %s"
