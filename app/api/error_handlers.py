15899fdaa209210cb78b258f2700bd1d47baaafb
nikos <nikgiannsm@gmail.com>
2024-11-25 21:17:36 +0200

    Fixed custom error handling response message format

--- app/api/error_handlers.py
+++ app/api/error_handlers.py
@@ -5,24 +5,24 @@ def error_handling(app):
     
     @app.errorhandler(NotFoundError)
     def not_found_error_response(e):
-        return jsonify({"message": e.message}), 400
+        return jsonify(e.message), 400
     
     @app.errorhandler(InvalidPasswordError)
     def invalid_password_error_response(e):
-        return jsonify({"message": e.message}), 400
+        return jsonify(e.message), 400
     
     @app.errorhandler(DuplicateEmailError)
     def duplicate_email_error_response(e):
-        return jsonify({"message": e.message}), 400
+        return jsonify(e.message), 400
     
     @app.errorhandler(WrongEmailFormatError)
     def wrong_email_format_error_response(e):
-        return jsonify({"message":e.message}), 400
+        return jsonify(e.message), 400
     
     @app.errorhandler(DuplicateUsernameError)
     def duplicate_username_error_response(e):
-        return jsonify({"message": e.message}), 400
+        return jsonify(e.message), 400
     
     @app.errorhandler(NotFoundInDbError)
     def not_found_in_db_error_response(e):
-        return jsonify({"message": e.message}), 400
+        return jsonify(e.message), 400
